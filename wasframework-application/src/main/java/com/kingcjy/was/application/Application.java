1ba01ed49c9ec20a026e6dbec0c38bccd0b5fae8
지니웍스 <genieworks@jiniwogseus-MacBook-Pro.local>
2019-11-04 18:15:28 +0900

    merge: master 브랜치 초기화

--- wasframework-application/src/main/java/com/kingcjy/was/application/Application.java
+++
@@ -1,9 +0,0 @@
-package com.kingcjy.was.application;
-
-import com.kingcjy.was.core.WasApplication;
-
-public class Application {
-    public static void main(String[] args) {
-        WasApplication.run(Application.class, args);
-    }
-}
--- wasframework-core/src/main/java/com/kingcjy/was/core/web/WebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.kingcjy.was.core.web;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import java.lang.reflect.InvocationTargetException;
-
-public interface WebApplicationInitializer {
-    void onStartup(ServletContext servletContext) throws ServletException;
-}
